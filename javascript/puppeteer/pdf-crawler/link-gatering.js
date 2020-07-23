243399d62cb4a9318f193c150650920f4f057d40
anjwoc <anjwoc@naver.com>
2020-07-24 00:42:52 +0900

    update

--- javascript/puppeteer/pdf-crawler/link-gatering.js
+++ javascript/puppeteer/pdf-crawler/link-gatering.js
@@ -4,16 +4,23 @@ const pupperteer = require('puppeteer');
 const crawler = async () => {
   const brower = await pupperteer.launch({headless: true});
   const page = await brower.newPage();
-  await page.goto();
-  await page.waitFor(3000);
+  await page.goto('', {waitUntil: 'networkidle2'});
+
   const links = await page.evaluate(() => {
     const nodes = document.querySelectorAll('.list-sub > .list-sub__item > div > a');
     return Array.from(nodes).map(n => {
       return {href: n.href};
     });
   });
-  if (links) {
-    await fs.writeFileSync('./data.json', JSON.stringify(links));
+  for await (let url of links) {
+    const linkPage = await brower.newPage();
+    const name = url.href.split('/');
+    await linkPage.goto(url.href, {waitUntil: ['load', 'networkidle2']});
+    await linkPage.pdf({
+      path: './pdf/' + name[name.length - 1] + '.pdf',
+      format: 'A4',
+    });
+    await linkPage.close();
   }
 
   await page.close();
