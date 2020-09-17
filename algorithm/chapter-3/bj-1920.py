19a9f8dbd9d23a2c645f78faab67ce36b6ce920c
anjwoc <anjwoc@naver.com>
2020-09-17 15:27:20 +0000

    backjoon 1920(수 찾기), sha-256 solved

---
+++ algorithm/chapter-3/bj-1920.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+n = int(input())
+arr1 = set(map(int, input().split()))
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+for i in arr2:
+    if i in arr1:
+        print(1)
+    else:
+        print(0)
